c5e369659a15e5caf679d6948d467493163f26c5
Valeriaadriana <38529370+Valeriaadriana@users.noreply.github.com>
2019-01-07 23:55:10 +0200

    Add files via upload

---
+++ disciplina.h
@@ -0,0 +1,11 @@
+#pragma once
+class Discipline
+{
+public:
+	Discipline(const std::string & subject, const Discipline type, const std::string& specialization)
+
+		m_Type(type);
+	m_Specialization(specialization);
+private:
+	m_Subject(subject),
+};
